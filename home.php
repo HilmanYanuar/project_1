922526167a1e3f2fd98e162b67a10fda18e001f1
hilmanyanuar <hilmanyanuar90@gmail.com>
2016-11-10 16:44:33 +0700

    Hilman Yanuar Commit 3

--- home.php
+++ home.php
@@ -1,12 +1,3 @@
 <?php
-<<<<<<< HEAD
-	echo "Ini codingannya Hilman Yanuar!";
-=======
-
-	echo "ane fikri bro";
-	echo "hilman yanuar";
-	echo "hilman yanuarrrr";
-	echo "fikri";
-
->>>>>>> 853983711c4546e4e3bf4105f1c640e5855f00d3
+	echo "Ini codingannya Hilman Yanuar!"
 ?>
